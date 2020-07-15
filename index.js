76952ff8aca60c9d9491d9dd6a1d23474772650f
Carlos Robles <crobles@dbamastery.com>
2020-07-14 18:48:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Updated Hello World!");
+    response.end("Updated Hello World as of today 07/14");
 });
 
 const port = process.env.PORT || 1337;
